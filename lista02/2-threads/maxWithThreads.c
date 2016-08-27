4102c6889b95d7cbcfa09b66b8f42d9b1cff929f
Artur Bersan de Faria <artur_bersan@hotmail.com>
2016-08-27 20:49:56 -0300

    Create threads

--- lista02/2-threads/maxWithThreads.c
+++ lista02/2-threads/maxWithThreads.c
@@ -2,32 +2,34 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include"dinamicVector.h"
-/* #include"maxWithThreads.h" */
-/*  */
-/* int *init_vector_w(int argc){ */
-/*   #<{(| int *w = malloc(sizeof(int)*(argc-1*argc-2)/2); |)}># */
-/*   int i; */
-/*   pthread_t *w = (pthread_t *) malloc(sizeof(pthread_t)*argc-1); */
-/*   for(i=0;i>argc-1;i++){ */
-/*     pthread_create(&w[i], NULL,init_with_i, &i); */
-/*   } */
-/*   pthread(); */
-/*  */
-/* } */
-/*  */
-/* void * init_with_i(void *args){ */
-/*  */
-/*  */
-/* } */
+#include"maxWithThreads.h"
+
+void *init_vector_w(int argc){
+  /* int *w = malloc(sizeof(int)*(argc-1*argc-2)/2); */
+  int j,i;
+  pthread_t *w = (pthread_t *) malloc(sizeof(pthread_t)*argc-1);
+  for(i=0;i<argc-1;i++){
+    pthread_create(&w[i], NULL,init_with_i, &i);
+  }
+  for(j=0;j<argc-1;j++){
+    pthread_join(w[j], NULL);
+  }
+
+}
+
+void * init_with_i(void *args){
+  printf("Args = %d\n", *((int *)args) );
+}
 
 int main(int argc, char** argv){
 
-  if(argc>1){
-    array collection = parse_string_to_int(argc,argv);
-    print_collection(collection);
-  } else {
-    printf("This program needs more parameters\n");
-  }
+  init_vector_w(argc);
+  /* if(argc>1){ */
+  /*   array collection = parse_string_to_int(argc,argv); */
+  /*   print_collection(collection); */
+  /* } else { */
+  /*   printf("This program needs more parameters\n"); */
+  /* } */
 
   return 0;
 }
--- lista02/2-threads/maxWithThreads.h
+++ lista02/2-threads/maxWithThreads.h
@@ -1,7 +1,7 @@
 #ifndef MAX_WITH_THREADS
 #define MAX_WITH_THREADS
 
-int *init_vector_w(int argc);
+void *init_vector_w(int argc);
 void * init_with_i(void *args);
 
 #endif
